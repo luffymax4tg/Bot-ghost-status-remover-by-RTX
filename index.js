da757a3330b6ce7a44de4d776b62ffd3c923a89d
luffymax4tg <166718518+luffymax4tg@users.noreply.github.com>
2024-04-11 18:03:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["playing gta samp","Watching members"];
 
 
 let currentIndex = 0;
